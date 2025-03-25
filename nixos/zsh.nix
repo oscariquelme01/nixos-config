a35b530c04875d50067c157de6a3784d46c4fc0a
Oscar <oscariquelmejato01@gmail.com>
2025-03-25 16:15:56 +0000

    probando

---
+++ nixos/zsh.nix
@@ -0,0 +1,13 @@
+programs.zsh = {
+  enable = true;
+  enableCompletion = true;
+  autosuggestions.enable = true;
+  syntaxHighlighting.enable = true;
+
+  shellAliases = {
+    ll = "ls -l";
+    update = "sudo nixos-rebuild switch";
+  };
+  history.size = 10000;
+};
+
