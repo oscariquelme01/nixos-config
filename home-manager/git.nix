7025181ca50c28d743757d79f575fc9fdf06bc01
Oscar <oscariquelmejato01@gmail.com>
2025-04-08 11:32:05 +0200

    feat: configure git

---
+++ home-manager/git.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+{
+  programs.git = {
+    enable = true;
+
+    userName = "Frost-Phoenix";
+    userEmail = "67cyril6767@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential.helper = "store";
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
+    };
+
+    delta = {
+      enable = true;
+      aliases = {
+        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
+        d = "diff HEAD{'^',}";
+      };
+      options = {
+        line-numbers = true;
+        side-by-side = true;
+        diff-so-fancy = true;
+        navigate = true;
+      };
+    };
+  };
+}
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,6 +15,7 @@
     # You can also split up your configuration and import pieces of it here:
     ./shell
     ./browser
+    ./git.nix
   ];
 
   nixpkgs = {
